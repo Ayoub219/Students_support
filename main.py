2fdf790d59f002cd89b98a5654fdea75d1a09f2a
Ayoub_Achahboun <achahbounayoub@gmail.com>
2024-11-14 14:01:54 +0100

    Avancement2

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from src.analyse_exploratoire import AnalyseEploratoireDonnees
 from src.data_processing import PretraitementsDonnees
 from src.modalite_chiffre import ModaliteeChiffre
 from src.tests_statistiques import TestsStatistiques
+from src.clustering_kmeans import Clustering
 
 def main():
     
@@ -13,27 +14,27 @@ def main():
     categorical_columns = data.select_dtypes(include=['object']).columns
     
     
-    analyse_exploratoire = AnalyseEploratoireDonnees(data)
-    
     
     pretraitement = PretraitementsDonnees(data)
     
     
     #Encodage des variables catégorielles
     data_encoded = pretraitement.encodage_var_categorielles(categorical_columns)
-    print(data_encoded.head())
     
-    test = TestsStatistiques(data_encoded)
-    anova = test.test_anova(categorical_columns)
-    print(anova)
+    data_encoded_new = data_encoded.copy()
+    data_encoded_new = data_encoded.drop(columns = ['Fedu', 'Mjob', 'Walc'])
     
     #Scaling des données 
-    #data_scaled = pretraitement.scale_data(data_encoded)
-    #print(data_scaled)
+    data_scaled = pretraitement.scale_data(data_encoded_new)
+    print(data_scaled)
         
-    #Dictionnaire pour attribuer chaque modalité de la variable à son chiffre correspondant    
-    #correspondance_modalite_chiffre = ModaliteeChiffre(data)
-    #print(correspondance_modalite_chiffre.dict_modalité_chiffre(categorical_columns))
+    #kmeans clustering
+    Kmeans_clustering = Clustering(data_encoded_new, data_scaled)
+    data_with_cluster = Kmeans_clustering.kmeans(6)
+    print(data_with_cluster.head())
+    
+    data_kmeans_analisis = data_with_cluster.groupby("cluster").mean() 
+    print(data_kmeans_analisis)
     
     
 if __name__ == "__main__":
---
+++ src/clustering.py
@@ -0,0 +1,31 @@
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+class Clustering:
+    
+    def __init__(self, data_encoded_new, data_scaled):
+        self.data_encoded_new = data_encoded_new
+        self.data_scaled = data_scaled
+        #self.n_clusters = n_clusters
+        
+    def regle_coude(self):
+        inertias = []
+        K_range = range(1, 11)  # Tester de 1 à 10 clusters
+
+        for k in K_range:
+            kmeans = KMeans(n_clusters=k, random_state=0)
+            kmeans.fit(self.data_scaled)
+            inertias.append(kmeans.inertia_)
+        # Tracer le graphe de la règle du coude
+        plt.figure(figsize=(8, 5))
+        plt.plot(K_range, inertias, marker='o')
+        plt.xlabel('Nombre de clusters k')
+        plt.ylabel("Inertie (somme des distances au carré)")
+        plt.title("Méthode du coude pour déterminer le nombre optimal de clusters")
+        plt.show()
+        
+    def kmeans(self, n_clusters):
+        """Applique KMeans pour effectuer le clustering."""
+        kmeans = KMeans(n_clusters)  # Ajuste le nombre de clusters selon tes besoins
+        self.data_encoded_new['cluster'] = kmeans.fit_predict(self.data_scaled)
+        return self.data_encoded_new
